dd41722cd9a9e2892d51c32fd963aa70735a3a7a
David Benko <dbenko@prndl.us>
2014-01-29 22:03:24 -0500

    First Successful SOAP Request

--- app/models/home_energy_score.rb
+++ app/models/home_energy_score.rb
@@ -16,16 +16,54 @@ module HomeEnergyScore
   # homeowner with a list of recommended energy improvements and the associated
   # cost savings estimates.
   #
+  # http://trk-hesapici-sb.hescloud.net/st_api/wsdl
   #
-  #  http://trk-hesapici-sb.hescloud.net/st_api/wsdl
   #
+  # Docs
+  # https://developers.buildingsapi.lbl.gov/hescore/documentation/hescore-api-methods
+  #
+
+  @@assessment_types= {
+      :initial => 'initial',
+      :final => 'final',
+      :qa => 'qa',
+      :alternative => 'alternative',
+      :test => 'test',
+      :corrected => 'corrected'
+  }
+
+  def self.assessment_types
+    @@assessment_types
+  end
 
   class HomeEnergyScore
     extend Savon::Model
     client wsdl: 'http://trk-hesapici-sb.hescloud.net/st_api/wsdl'
 
+    operations :submit_address, :submit_inputs, :calculate, :commit_results,
+               :generate_label, :retrieve_inputs, :retrieve_label_results,
+               :retrieve_extended_results, :retrieve, :retrieve_recommendations,
+               :retrieve_buildings_by_id, :delete_buildings_by_id, :archive_buildings_by_id,
+               :retrieve_buildings_by_address, :validate_inputs
+
     def list_operations
       client.operations
+    end
+
+    # Creates a new building
+    def submit_address(address='', city='', state='', zip_code='', assessment_type='')
+      params = {
+          'building_address' => {
+            'user_key' => ENV['HOME_ENERGY_SCORE_USER_KEY'],
+            'qualified_assessor_id' => ENV['HOME_ENERGY_SCORE_QAID'],
+            'address' => address,
+            'city' => city,
+            'state' => state,
+            'zip_code' => zip_code,
+            'assessment_type' => assessment_type
+          }
+      }
+      super(message: params)
     end
 
   end
